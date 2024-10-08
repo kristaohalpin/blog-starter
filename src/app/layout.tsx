3352d10e32fd4f93476cf24995a1e51c83791898
KRISTA O'HALPIN <126205255+kristaohalpin@users.noreply.github.com>
2024-10-08 12:31:12 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `Krista O'Halpin`,
+  description: `Just an artist typing some things using Next.JS with ${CMS_NAME}.`,
   openGraph: {
-    images: [HOME_OG_IMAGE_URL],
+    images: [],
   },
 };
 
